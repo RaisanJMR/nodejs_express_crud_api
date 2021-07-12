f1860bd6344c5f0155e95e4034a08e5797ab8293
RaisanJMR <raisanjmr@protonmail.com>
2021-07-12 12:27:03 +0530

    get method

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,48 @@
 import express from 'express';
-
+import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
-const users = [
-  {
-    firstName: 'john',
-    lastName: 'Doe',
-    age: '20',
-  },
-  {
-    firstName: 'james',
-    lastName: 'haddin',
-    age: '14',
-  },
+let users = [
+  // {
+  //   firstName: 'john',
+  //   lastName: 'Doe',
+  //   age: '20',
+  // },
+  // {
+  //   firstName: 'james',
+  //   lastName: 'haddin',
+  //   age: '14',
+  // },
 ];
+// **************************************************
 // by default all routes here starts with /users
 // GET
 // browser will only make get request
 router.get('/', (req, res) => {
-  console.log(users);
   res.send(users);
 });
+// **************************************************
 // POST
 // send data from front end to database
 // when we do post request we have req.body
 router.post('/', (req, res) => {
-  console.log('request body',req.body);
-  res.send('POST ROUTER REACHED');
+  const user = req.body;
+  // push the body of the post request to array
+  users.push({ ...user, id: uuidv4() });
+  res.send(`user with name ${user.firstName} added to DB`);
 });
-
+// **************************************************
+// get a user with specific :id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+// **************************************************
+// DELETE
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  // when user.id != id it returns true and keeps that data.
+  users = users.filter((user) => user.id != id);
+  res.send(`user with id ${id} deleted from database`)
+});
 export default router;
